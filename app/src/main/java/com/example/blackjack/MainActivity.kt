0403269f9f460174a716d6b2dd6823e75a083f37
Salvatore <salvatore.lamarcab@gmail.com>
2023-11-11 14:10:16 -0500

    Added Hit Button to screen

--- app/src/main/java/com/example/blackjack/MainActivity.kt
+++ app/src/main/java/com/example/blackjack/MainActivity.kt
@@ -2,13 +2,32 @@ package com.example.blackjack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val game = BlackJack();
+
+        val hit: Button = findViewById(R.id.hitButton);
+        hit.setOnClickListener {
+            dealPlayerCard(game);
+        };
+
+
+
 
 
     }
 }
+
+fun dealPlayerCard(game: BlackJack) {
+    game.dealCardPlayer(game.generateRandom());
+}
+
+
+
